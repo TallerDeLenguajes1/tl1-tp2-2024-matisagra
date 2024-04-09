f9d76e9277bd1dc3bb0e5978ca215277ad3a7af6
matisagra <sagramatias04@gmail.com>
2024-04-09 16:36:15 -0300

    quinto commit

---
+++ tp2_4.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+struct compu {
+int velocidad;//entre 1 y 3 GHz
+int anio;//entre 2015 y 2023
+int cantidad;//entre 1 y 8
+char *tipo_cpu; //valores del arreglo tipos
+};typedef pc;
+
+int main(){
+    char tipos[6][10]={"Intel", "AMD", "Celeron", "Athlon", "Core", "Pentium"};
+
+     
+     pc computadora = {1,2022,5,*tipos};
+
+}
+
